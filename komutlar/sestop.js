de5b4d9df61b6c0f2f607d420a5c25e0dfa8f783
Glitch (hello-express) <none>
2022-02-01 13:46:26 +0000

    🐿😹 Checkpoint ./komutlar/sestop.js:33987954/5708

--- komutlar/sestop.js
+++ komutlar/sestop.js
@@ -1,17 +1,29 @@
-const Discord = require("discord.js");
-exports.run = async (client, message, args) => {
-const embed1 = new Discord.MessageEmbed()
-.setAuthor('${message.author} kişinin ses ve yazı aktifliği!', message.author.displayAvatarURL())
-.setDescription(`Bu komut sayesinde k! \n **Zaman:** 10/01/2022 \n \n **Oyun Açıklaması:** Alanda mayınlara rastlamadan tüm boş kareleri bulmaktır. Karelere tıklayınca çıkan sayılar ise karenin etrafındaki mayın sayısının toplamını gösterir.`)
-.setColor("")
-.setFooter('Görüntüleyen: ' + message.author.tag, message.author.displayAvatarURL())
-message.channel.send(embed1)
+const Discord = require('discord.js')
+const ms = require('ms')
+const db = require('quick.db')
+
+    exports.run = async(client, message, args) => {
+    if (message.channel.type !== 'dm') {
+    
+        let kullanıcı = message.mentions.members.first();
+        let sebep = args.slice(1).join(' ')
+
+            const cmfmute = new Discord.MessageEmbed()
+            .setAuthor("%{message.author}!", message.author.displayAvatarURL())
+            .setDescription(`Bu komut sayesinde kişinin ses aktifliğini görebilirsin. \n\n Toplam Ses: **.** \n Haftalık Ses: **.** \n Günlük Ses: **.**`)
+            .setColor("#04F9EC")
+            .setFooter('Yetkili: ' + message.author.tag, message.author.displayAvatarURL())
+            message.channel.send(cmfmute)
+
+        }
 }
 exports.conf = {
-  aliases: ["mayıntarlası", "mayın-tarlası", "mayin", "mayintarlasi"],
-  permLevel: 0
-};
+    enabled: true,
+    guildOnly: false,
+    aliases: ['uyarıver','Uyarıver','UyarıVer','Uyarı-ekle','uyarı-ekle'],
+    permLevel: 0
+}
 
 exports.help = {
-  name: "voice"
-};
+    name: 'voice'
+}
